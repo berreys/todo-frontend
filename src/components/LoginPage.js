56ce9e63a68b6cebe3b4aa1e27420cb17ebb0809
berreys <saberrey@gmail.com>
2024-10-15 22:49:58 -0600

    functionality, UI complete

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import './LoginStyle.css';
 
 const LoginPage = () => {
     const [username, setUsername] = useState('');
@@ -16,18 +17,24 @@ const LoginPage = () => {
     }
 
     return (
-        <div>
-            <h1>Login</h1>
-            <form onSubmit={handleSubmit}>
-                <input
-                    type="text"
-                    value={username}
-                    onChange={handleInputChange}
-                    placeholder="Enter username"
-                    required
-                />
-                <button type="submit">Log In</button>
-            </form>
+        <div className="mainContainer">
+            <h1 className="h1">Sam Berrey's To-Do List</h1>
+            <div className='loginContainer'>
+                <h2 className="h2">Login or Register</h2>
+                <p className="p">If the entered username is not already registered,<br></br> an account will be created.</p>
+                <form onSubmit={handleSubmit} className="form">
+                
+                    <input
+                        type="text"
+                        value={username}
+                        onChange={handleInputChange}
+                        placeholder="Enter username"
+                        required
+                        className="input"
+                    />
+                    <button type="submit" className="button">Log In</button>
+                </form>
+            </div>
         </div>
     );
 }
--- src/components/TodoPage.js
+++ src/components/TodoPage.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect, useRef } from 'react';
 import { useLocation } from 'react-router-dom';
+import './TodoStyle.css';
 
 const TodoPage = () => {
     const location = useLocation();
@@ -7,6 +8,11 @@ const TodoPage = () => {
     const [userData, setUserData] = useState(null);
     const [loading, setLoading] = useState(true);
     const hasFetchedRef = useRef(false);
+    const [showPending, setShowPending] = useState(true);
+    const [showComplete, setShowComplete] = useState(true);
+    const [newTaskText, setNewTaskText] = useState('');
+    const [editIndex, setEditIndex] = useState(null);
+    const [editText, setEditText] = useState('');
 
     useEffect(() => {
         if (username && !hasFetchedRef.current) {
@@ -15,19 +21,14 @@ const TodoPage = () => {
         }
     }, [username]);
 
-    var showPending = true;
-    var showComplete = true;
-
     const fetchUserData = async (usernameParam) => {
         setLoading(true);
         try {
-            console.log("USERNAME:", usernameParam);
             const response = await fetch(`https://todo-backend-b066bc9bfdc1.herokuapp.com/login/${usernameParam}`);
             if (!response.ok) {
                 throw new Error('Error logging in');
             }
             const data = await response.json();
-            console.log("DATA:", data);
             setUserData(data);
         } catch (error) {
             console.error('Error logging in:', error);
@@ -41,131 +42,240 @@ const TodoPage = () => {
         try {
             const response = await fetch(`https://todo-backend-b066bc9bfdc1.herokuapp.com/completeTask`, {
                 method: 'PUT',
-                headers: {'Content-Type': 'application/json'},
-                body: JSON.stringify({"username": username, "index": index})
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({ "username": username, "index": index })
             });
-            console.log("BEFORE", userData);
             if (!response.ok) {
                 throw new Error('Error completing todo');
-            }
-            else {
+            } else {
                 setUserData((prevData) => {
                     const updatedTasks = [...prevData.tasks];
-                    updatedTasks[index].state = 'complete'; // Toggle completion
+                    updatedTasks[index].state = 'complete';
                     return { ...prevData, tasks: updatedTasks };
-                })
+                });
             }
-            console.log("AFTER", userData);
-        }
-        catch(error) {
+        } catch (error) {
             console.error('Error completing todo:', error);
         }
     };
-    const editItem = async (index, newText) => {
+
+    const handleEditItemClick = (index, text) => {
+        setEditIndex(index);
+        setEditText(text);
+    };
+
+    const handleEditSubmit = async (index) => {
         try {
-            console.log(newText);
             const response = await fetch(`https://todo-backend-b066bc9bfdc1.herokuapp.com/editTask`, {
                 method: 'PUT',
-                headers: {'Content-Type': 'application/json'},
-                body: JSON.stringify({"username": username, "index": index, "newText": newText})
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({ "username": username, "index": index, "newText": editText })
             });
-            if(!response.ok) {
+            if (!response.ok) {
                 throw new Error('Error editing todo');
-            }
-            else {
+            } else {
                 setUserData((prevData) => {
                     const updatedTasks = [...prevData.tasks];
-                    updatedTasks[index].text = newText;
-                    return {...prevData, tasks: updatedTasks}
+                    updatedTasks[index].text = editText;
+                    return { ...prevData, tasks: updatedTasks };
                 });
+                setEditIndex(null); // Close the editing input
             }
-        }
-        catch(error) {
+        } catch (error) {
             console.error('Error editing todo:', error);
         }
     };
+
     const removeItem = async (index) => {
-        try{
+        try {
             const response = await fetch(`https://todo-backend-b066bc9bfdc1.herokuapp.com/removeTask`, {
                 method: 'DELETE',
-                headers: {'Content-Type': 'application/json'},
-                body: JSON.stringify({"username": username, "index": index})
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({ "username": username, "index": index })
             });
-            if(!response.ok) {
-                throw new Error('Error editing todo');
-            }
-            else {
+            if (!response.ok) {
+                throw new Error('Error removing todo');
+            } else {
                 setUserData((prevData) => {
                     const updatedTasks = [...prevData.tasks];
                     updatedTasks.splice(index, 1);
-                    return {...prevData, tasks: updatedTasks}
+                    return { ...prevData, tasks: updatedTasks };
                 });
             }
-        }
-        catch(error) {
+        } catch (error) {
             console.error('Error removing todo:', error);
         }
     };
-    const addItem = async (text) => {
+
+    const handleAddTaskSubmit = async () => {
+        if(newTaskText === '') return;
         try {
             const response = await fetch(`https://todo-backend-b066bc9bfdc1.herokuapp.com/addTask`, {
                 method: 'PUT',
-                headers: {'Content-Type': 'application/json'},
-                body: JSON.stringify({"username": username, "task": {"state": "pending", "text": text}})
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({ "username": username, "task": { "state": "pending", "text": newTaskText } })
             });
-            if(!response.ok) {
+            if (!response.ok) {
                 throw new Error('Error adding todo');
-            }
-            else{
+            } else {
                 setUserData((prevData) => {
                     const updatedTasks = [...prevData.tasks];
-                    updatedTasks.push({"state": "pending", "text": text});
-                    return {...prevData, tasks: updatedTasks}
-                })
+                    updatedTasks.push({ "state": "pending", "text": newTaskText });
+                    return { ...prevData, tasks: updatedTasks };
+                });
+                setNewTaskText(''); // Clear the input field after submission
             }
-        }
-        catch(error) {
+        } catch (error) {
             console.error('Error adding todo:', error);
         }
-    }
-    const changeShowPending = () => {
-        showPending = !showPending;
-    }
+    };
+
+    const handleShowPendingChange = () => {
+        setShowPending((prev) => !prev);
+    };
+
+    const handleShowCompleteChange = () => {
+        setShowComplete((prev) => !prev);
+    };
 
     return (
-        <div>
-            <h1>{username}'s Todo List</h1>
+        <div className='div'>
+            <h1 className='header'>{username}'s To-Do List</h1>
             {loading ? (
-                <p>Loading user data...</p>
+                <p className='loading-text'>Loading user data...</p>
             ) : userData ? (
-                <div>
-                    <input type="checkbox" id="showPending"/>
-                        <label for="showPending" onChange={changeShowPending}>Show incomplete tasks</label>
-                    <ul>
+                <div className='body-div'>
+                    <div className='filter-buttons'>
+                        <input type="checkbox" id="showPending" onChange={handleShowPendingChange} checked={showPending} />
+                        <label htmlFor="showPending">Show incomplete tasks</label>
+                        <input type="checkbox" id="showComplete" onChange={handleShowCompleteChange} checked={showComplete} />
+                        <label htmlFor="showComplete">Show complete tasks</label>
+                    </div>
+                    <ul className='list'>
                         {userData.tasks.map((todo, index) => (
-                            <li key={index} className={(todo.state === 'pending' && !showPending) || (todo.state === 'complete' && !showComplete) ? 'hide' : ''} >
-                                <span
-                                    style={{
-                                        textDecoration: todo.state === 'complete' ? 'line-through' : 'none',
-                                    }}
-                                >
-                                    {todo.text}
-                                </span>
-                                <button onClick={() => completeItem(index)} className={todo.state === 'pending' ? '' : 'hide'}>
-                                    {todo.state === 'complete' ? 'Undo' : 'Complete'}
-                                </button>
-                                <button onClick={() => editItem(index, prompt('Edit your todo:', todo.text))}>
-                                    Edit
-                                </button>
-                                <button onClick={() => removeItem(index)}>
-                                    Remove
-                                </button>
+                            <li key={index} className={(todo.state === 'pending' && !showPending) || (todo.state === 'complete' && !showComplete) ? 'hide' : 'task'}>
+                                {editIndex === index ? (
+                                    <div className="edit-div">
+                                        <input
+                                            type="text"
+                                            value={editText}
+                                            onChange={(e) => setEditText(e.target.value)}
+                                        />
+                                        
+                                        <button onClick={() => setEditIndex(null)} className='remove-btn'>
+                                            <svg
+                                                xmlns="http://www.w3.org/2000/svg"
+                                                viewBox="0 0 24 24"
+                                                fill="none"
+                                                stroke="currentColor"
+                                                strokeWidth="2"
+                                                strokeLinecap="round"
+                                                strokeLinejoin="round"
+                                                width="24"
+                                                height="24"
+                                            >
+                                                <line x1="18" y1="6" x2="6" y2="18" />
+                                                <line x1="6" y1="6" x2="18" y2="18" />
+                                            </svg>
+                                        </button>
+                                        <button onClick={() => handleEditSubmit(index)} className='complete-btn'>
+                                            <svg
+                                                xmlns="http://www.w3.org/2000/svg"
+                                                viewBox="0 0 24 24"
+                                                fill="none"
+                                                stroke="currentColor"
+                                                strokeWidth="2"
+                                                strokeLinecap="round"
+                                                strokeLinejoin="round"
+                                                width="24"
+                                                height="24"
+                                            >
+                                                <polyline points="20 6 9 17 4 12" />
+                                            </svg>
+                                        </button>
+                                    </div>
+                                ) : (
+                                    <span style={{ textDecoration: todo.state === 'complete' ? 'line-through' : 'none' }}>
+                                        {todo.text}
+                                    </span>
+                                )}
+                                {editIndex !== index && (
+                                    <>
+                                        <button onClick={() => removeItem(index)} className="remove-btn">
+                                            <svg
+                                                xmlns="http://www.w3.org/2000/svg"
+                                                viewBox="0 0 24 24"
+                                                fill="none"
+                                                stroke="currentColor"
+                                                strokeWidth="2"
+                                                strokeLinecap="round"
+                                                strokeLinejoin="round"
+                                                width="24"
+                                                height="24"
+                                            >
+                                                <line x1="18" y1="6" x2="6" y2="18" />
+                                                <line x1="6" y1="6" x2="18" y2="18" />
+                                            </svg>
+                                        </button>
+                                        <button onClick={() => handleEditItemClick(index, todo.text)} className={todo.state === 'pending' ? 'edit-btn' : 'hide'}>
+                                            <svg
+                                                xmlns="http://www.w3.org/2000/svg"
+                                                viewBox="0 0 24 24"
+                                                fill="none"
+                                                stroke="currentColor"
+                                                strokeWidth="2"
+                                                strokeLinecap="round"
+                                                strokeLinejoin="round"
+                                                width="24"
+                                                height="24"
+                                            >
+                                              <path d="M12 20h9" />
+                                              <path d="M16.5 3l4.5 4.5-12 12H4v-4l12-12z" />
+                                            </svg>
+                                        </button>
+                                        <button onClick={() => completeItem(index)} className={todo.state === 'pending' ? 'complete-btn' : 'hide'}>
+                                            <svg
+                                                xmlns="http://www.w3.org/2000/svg"
+                                                viewBox="0 0 24 24"
+                                                fill="none"
+                                                stroke="currentColor"
+                                                strokeWidth="2"
+                                                strokeLinecap="round"
+                                                strokeLinejoin="round"
+                                                width="24"
+                                                height="24"
+                                            >
+                                                <polyline points="20 6 9 17 4 12" />
+                                            </svg>
+                                        </button>
+                                    </>
+                                )}
                             </li>
                         ))}
                     </ul>
-                    <button onClick={() => addItem(prompt('Enter your todo:'))}>
-                        Add Item
-                    </button>
+                    <div className="add-div">
+                        <input
+                            type="text"
+                            value={newTaskText}
+                            onChange={(e) => setNewTaskText(e.target.value)}
+                            placeholder="Enter new task"
+                        />
+                        <button onClick={handleAddTaskSubmit} className="complete-btn">
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                viewBox="0 0 24 24"
+                                fill="none"
+                                stroke="currentColor"
+                                strokeWidth="2"
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                width="24"
+                                height="24"
+                            >
+                                <polyline points="20 6 9 17 4 12" />
+                            </svg>
+                        </button>
+                    </div>
                 </div>
             ) : (
                 <p>No user data available.</p>
